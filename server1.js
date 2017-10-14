2147d45f70111d38a8b25036907b6e069fc9c1bc
Shreejit Chaudhury <SChaudhury@walmartlabs.com>
2017-10-14 18:50:12 +0530

    first commit

---
+++ server1.js
@@ -0,0 +1,18 @@
+/**
+ * Created by schaud3 on 10/12/17.
+ */
+var express = require('express');
+var router = express.Router();
+var app = express();
+
+router.get('/', (req, res, next) => {
+    res.send({hi:'there'});
+});
+
+app.use('/', router);
+try {
+    const PORT = process.env.PORT;
+    app.listen(PORT);
+} catch (error) {
+    console.log(error);
+}
---
+++ test.js
@@ -0,0 +1,7 @@
+/**
+ * Created by schaud3 on 10/10/17.
+ */
+const list = ['1','2','3'];
+for (item in list) {
+    console.log(item);
+}
